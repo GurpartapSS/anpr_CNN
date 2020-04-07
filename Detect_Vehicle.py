652adca9c4c384a72e79b3455a16613ae66ca15a
Gurpartap <gurpartapsingh.sarkaria@gmail.com>
2020-04-07 18:39:54 -0400

    r

--- Detect_Vehicle.py
+++ Detect_Vehicle.py
@@ -1,14 +1,13 @@
 # USAGE
 # python yolo.py --image images/<image name>.jpg --yolo yolo-coco
 
-# import the necessary packages
+
 import numpy as np
 import argparse
 import time
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image")
@@ -20,34 +19,25 @@ ap.add_argument("-t", "--threshold", type=float, default=0.3,
 	help="threshold when applyong non-maxima suppression")
 args = vars(ap.parse_args())
 
-# load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
 
-# initialize a list of colors to represent each possible class label
+
 np.random.seed(42)
 COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
 	dtype="uint8")
 
-# derive the paths to the YOLO weights and model configuration
 weightsPath = os.path.sep.join([args["yolo"], "yolov3.weights"])
 configPath = os.path.sep.join([args["yolo"], "yolov3.cfg"])
 
-# load our YOLO object detector trained on COCO dataset (80 classes)
 print("[INFO] loading YOLO from disk...")
 net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
 
-# load our input image and grab its spatial dimensions
 image = cv2.imread(args["image"])
 (H, W) = image.shape[:2]
 
-# determine only the *output* layer names that we need from YOLO
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-
-# construct a blob from the input image and then perform a forward
-# pass of the YOLO object detector, giving us our bounding boxes and
-# associated probabilities
 blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
 	swapRB=True, crop=False)
 net.setInput(blob)
@@ -55,32 +45,19 @@ start = time.time()
 layerOutputs = net.forward(ln)
 end = time.time()
 
-# show timing information on YOLO
-print("[INFO] YOLO took {:.6f} seconds".format(end - start))
-
-# initialize our lists of detected bounding boxes, confidences, and
-# class IDs, respectively
 boxes = []
 confidences = []
 classIDs = []
 
-# loop over each of the layer outputs
 for output in layerOutputs:
 	# loop over each of the detections
 	for detection in output:
-		# extract the class ID and confidence (i.e., probability) of
-		# the current object detection
 		scores = detection[5:]
 		classID = np.argmax(scores)
 		confidence = scores[classID]
 
-		# filter out weak predictions by ensuring the detected
-		# probability is greater than the minimum probability
 		if confidence > args["confidence"]:
-			# scale the bounding box coordinates back relative to the
-			# size of the image, keeping in mind that YOLO actually
-			# returns the center (x, y)-coordinates of the bounding
-			# box followed by the boxes' width and height
+			
 			box = detection[0:4] * np.array([W, H, W, H])
 			(centerX, centerY, width, height) = box.astype("int")
 
@@ -89,21 +66,17 @@ for output in layerOutputs:
 			x = int(centerX - (width / 2))
 			y = int(centerY - (height / 2))
 
-			# update our list of bounding box coordinates, confidences,
-			# and class IDs
 			if(classID == 2):
 
 				boxes.append([x, y, int(width), int(height)])
 				confidences.append(float(confidence))
 				classIDs.append(classID)
 				
-# apply non-maxima suppression to suppress weak, overlapping bounding
-# boxes
 idxs = cv2.dnn.NMSBoxes(boxes, confidences, args["confidence"],
 	args["threshold"])
 
-# ensure at least one detection exists
 if len(idxs) > 0:
+	count = 0
 	# loop over the indexes we are keeping
 	for i in idxs.flatten():
 		# extract the bounding box coordinates
@@ -116,6 +89,9 @@ if len(idxs) > 0:
 		text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
 		cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
 			0.5, color, 2)
+		to = image[y:y+h,x:x+w]
+		cv2.imshow("Image{}".format(count), to)
+		count +=1
 
 # show the output image
 cv2.imshow("Image", image)
